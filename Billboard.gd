021811a1d3e4f4f532ba6257e0b6c68bd999431c
markSarav <mark.saravia@gmail.com>
2024-04-04 15:46:16 -0400

    added an inside level added lights to inside level decorated inside level w/ lattices,holes, generic item containers added bed+billboard scenes as generic_item_conatiners added inside.gd with gate behavior

---
+++ Billboard.gd
@@ -0,0 +1,4 @@
+extends generic_item_container
+
+func hit():
+	print("billboard")
---
+++ scenes/levels/inside.gd
@@ -0,0 +1,8 @@
+extends LevelParent
+
+
+
+
+func _on_exit_gate_area_body_entered(_body):
+	var tween = create_tween()
+	tween.tween_property($Player,"speed",0,.5)
