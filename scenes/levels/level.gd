c6531f34aa33e652fbfb7d62cfaa5c299e8ccdf4
markSarav <mark.saravia@gmail.com>
2024-04-26 16:46:48 -0400

    added infestation area, more rooms, ground texture, added new decal and infestation nodes in parent scene

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -52,20 +52,4 @@ func _on_player_grenade_fired(pos,direction):
 
 #zoom in on player when house entered
 #tweens are faster ways of creating smaller animations w/o using the animation player node
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	#to play the tweens in parallel use
-	#tween.set_parallel(true)
-	#to change how fast the animation transtions you can use set_trans(Tween.TRANS_FUNCTION)
-	tween.tween_property($Player/Camera2D,"zoom",Vector2(1,1),1).set_trans(Tween.TRANS_QUAD)
-	#example of tween stacking (invis)
-	#we can also designate a starting position for the tween aniation instead of
-	#whatever position the player node started in by using from()
-	#tween.tween_property($Player,"modulate:a",0,2).from(.5)
-	#print("player has entered house")
-
-#return camera back to original zoom level
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D,"zoom",Vector2(.6,.6),1)
 
